ac498a0597cf00d7fdab8ea3f352a38c4d4b9803
twyle <lyceokoth@gmail.com>
2023-06-05 15:41:02 +0300

    refactors the response class.

--- youtube/resources/response_parsers.py
+++ youtube/resources/response_parsers.py
@@ -1,8 +1,36 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
 
-class ResponseParser(ABC):
-    @abstractmethod
+T = TypeVar('T')
+
+
+class ResponseParser(ABC, Generic[T]):
     def __call__(self, response: dict[str, str]) -> dict[str, str]:
-        pass      
+        parsed_items = self.parse_resource(response)
+        resources = [self.create_resource(item) for item in parsed_items]
+        return resources       
+    
+    def get_thumbnail(self, thumbnails: dict[str, str]) -> str:
+        thumbnail = ''
+        if thumbnails:
+            if thumbnails.get('standard'):
+                thumbnail = thumbnails.get('standard').get('url')
+            elif thumbnails.get('medium'):
+                thumbnail = thumbnails.get('medium').get('url')
+            elif thumbnails.get('high'):
+                thumbnail = thumbnails.get('high').get('url')
+            elif thumbnails.get('default'):
+                thumbnail = thumbnails.get('default').get('url')
+            elif thumbnails.get('maxres'):
+                thumbnail = thumbnails.get('maxres').get('url')
+        return thumbnail
+    
+    @abstractmethod
+    def parse_resource(self, result: dict[str, str]) -> dict[str, str]:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def create_resource(self, resource_data: dict[str, str]) -> T:
+        raise NotImplementedError()
     
     
--- youtube/resources/video/video_parser.py
+++ youtube/resources/video/video_parser.py
@@ -1,13 +1,8 @@
 from ..response_parsers import ResponseParser
 from ...models.video_model import Video
 
-class VideoResponseParser(ResponseParser):
-    def __call__(self, response: dict[str, str]) -> Video:
-        parsed_items = self.__parse_youtube_video(response)
-        videos = [self.__create_video(item) for item in parsed_items]
-        return videos
-    
-    def __parse_youtube_video(self, result: dict[str, str]) -> dict[str, str]:
+class VideoResponseParser(ResponseParser[Video]):    
+    def parse_resource(self, result: dict[str, str]) -> dict[str, str]:
         parsed_items = []
         for item in result['items']:
             parsed_video_details = dict()
@@ -17,7 +12,7 @@ class VideoResponseParser(ResponseParser):
             parsed_video_details['channel_id'] = item['snippet']['channelId']
             parsed_video_details['channel_title'] = item['snippet']['channelTitle']
             parsed_video_details['video_description'] = item['snippet']['description']
-            parsed_video_details['video_thumbnail'] = self.__get_thumbnail(item['snippet']['thumbnails'])
+            parsed_video_details['video_thumbnail'] = self.get_thumbnail(item['snippet']['thumbnails'])
             parsed_video_details['video_duration'] = item['contentDetails']['duration']
             parsed_video_details['views_count'] = item['statistics']['viewCount']
             parsed_video_details['likes_count'] = item['statistics']['likeCount']
@@ -25,22 +20,7 @@ class VideoResponseParser(ResponseParser):
             parsed_items.append(parsed_video_details)
         return parsed_items
     
-    def __get_thumbnail(self, thumbnails: dict[str, str]) -> str:
-        thumbnail = ''
-        if thumbnails:
-            if thumbnails.get('standard'):
-                thumbnail = thumbnails.get('standard').get('url')
-            elif thumbnails.get('medium'):
-                thumbnail = thumbnails.get('medium').get('url')
-            elif thumbnails.get('high'):
-                thumbnail = thumbnails.get('high').get('url')
-            elif thumbnails.get('default'):
-                thumbnail = thumbnails.get('default').get('url')
-            elif thumbnails.get('maxres'):
-                thumbnail = thumbnails.get('maxres').get('url')
-        return thumbnail
-    
-    def __create_video(self, video_data: dict[str, str]) -> Video:
+    def create_resource(self, video_data: dict[str, str]) -> Video:
         video = Video(
             video_id=video_data['video_id'],
             video_title=video_data['video_title'],
